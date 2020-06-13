d175b7a162fc130cc41aa8a88574021fc788f7cb
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-06-13 22:08:16 +0200

    Add log and balance check. Complete Stage #1

--- Simple Banking System/task/src/banking/Main.java
+++ Simple Banking System/task/src/banking/Main.java
@@ -1,21 +1,41 @@
 package banking;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
-    private static ArrayList<Account> accounts = new ArrayList<>();
+    private static final Set<Account> accountsSet = new HashSet<>();
     private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         runApplication();
         scanner.close();
     }
 
     private static void runApplication() {
-        String action = takeInput("main");
-        determineMainMenuAction(action);
+        determineMainMenuAction(takeInput("main"));
+    }
+
+    private static String takeInput(String menu) {
+        final String[] possibleActions = {"1", "2", "0"};
+        String action;
+        boolean hasAction = false;
+
+        do {
+            printMenu(menu);
+            action = scanner.next();
+            if (Arrays.asList(possibleActions).contains(action)) {
+                hasAction = true;
+
+            } else {
+                System.out.println("Provide valid input! (1 or 2 or 3)");
+            }
+        }
+        while (!(hasAction));
+        return action;
     }
 
     private static void printMenu(String menu) {
@@ -32,70 +52,21 @@ public class Main {
         }
     }
 
-    private static String takeInput(String menu) {
-        final String[] possibleActions = {"1", "2", "0"};
-        String action = "";
-        boolean hasAction = false;
-
-        do {
-            printMenu(menu);
-            action = scanner.next();
-            if (((Arrays.asList(possibleActions).contains(action)))) {
-                hasAction = true;
-
-            } else {
-                System.out.println("Provide valid input! (1 or 2 or 3)");
-            }
-        }
-        while (!(hasAction));
-
-
-        return action;
-    }
-
     private static void determineMainMenuAction(String action) {
         switch (action) {
-            case "1" : {
+            case "1": {
                 createAnAccount();
                 break;
             }
-            case "2" : {
+            case "2": {
                 logIntoAccount();
                 break;
             }
-            case "0" : {
+            case "0": {
                 exit();
                 break;
             }
         }
-    }
-
-    private static void determineLoggedUserAction(Account account, String action) {
-        switch (action) {
-            case "1" : {
-                checkBalance(account);
-                break;
-            }
-            case "2" : {
-                logOut();
-                break;
-            }
-            case "0" : {
-                exit();
-                break;
-            }
-        }
-    }
-
-    private static void checkBalance(Account account) {
-        System.out.println("\nBalance: " + account.getBalance() + "\n");
-        determineLoggedUserAction(account, takeInput("user"));
-    }
-
-    private static void logOut() {
-//      Logout;
-        System.out.println("You have successfully logged out!");
-        determineMainMenuAction(takeInput("main"));
     }
 
     private static void createAnAccount() {
@@ -118,25 +89,65 @@ public class Main {
         cardNumber = Long.parseLong(String.valueOf(cardNumber).concat(cardNumberString));
 
         Account account = new Account(cardNumber, pin, balance);
-        accounts.add(account);
+        accountsSet.add(account);
 
         System.out.println("\nYour card has been created\n" +
                 "Your card number:\n" +
                 cardNumber + "\n" +
                 "Your card PIN:\n" +
                 pin + "\n");
-        determineMainMenuAction(takeInput("main"));;
+        determineMainMenuAction(takeInput("main"));
     }
 
     private static void logIntoAccount() {
         System.out.println("\nEnter your card number: ");
-        System.out.println("...");
+        long cardNumber = scanner.nextLong();
         System.out.println("Enter your PIN: ");
-        System.out.println("...");
-        System.out.println("You have successfully logged in!");
-        System.out.println("You have successfully logged out!\n");
+        String pin = scanner.next();
+        Account loggedAccount = findAccount(cardNumber, pin);
+        System.out.println("\nYou have successfully logged in!\n");
+        determineLoggedUserAction(loggedAccount, takeInput("user"));
+    }
 
-        determineLoggedUserAction(accounts.get(0), takeInput("user"));
+    private static Account findAccount(long cardNumber, String pin) {
+        for (Account account : Main.accountsSet) {
+            long cn = account.getCardNumber();
+            String p = account.getPin();
+            if (cn == cardNumber && p.equals(pin)) {
+                return account;
+            } else {
+                System.out.println("\nWrong card number or PIN!\n");
+                determineMainMenuAction(takeInput("main"));
+            }
+        }
+        return null;
+    }
+
+    private static void determineLoggedUserAction(Account account, String action) {
+        switch (action) {
+            case "1": {
+                checkBalance(account);
+                break;
+            }
+            case "2": {
+                logOut();
+                break;
+            }
+            case "0": {
+                exit();
+                break;
+            }
+        }
+    }
+
+    private static void checkBalance(Account account) {
+        System.out.println("\nBalance: " + account.getBalance() + "\n");
+        determineLoggedUserAction(account, takeInput("user"));
+    }
+
+    private static void logOut() {
+        System.out.println("\nYou have successfully logged out!\n");
+        determineMainMenuAction(takeInput("main"));
     }
 
     private static void exit() {
